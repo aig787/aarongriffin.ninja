996c775857754f7878502ad94ef3541514e0c7ce
Aaron Griffin <aig787@gmail.com>
2014-11-26 20:45:02 +0000

    Updated submodule and build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ssh-agent bash -c 'ssh-add /home/aaron/.ssh/github; git pull origin master --force'
+ssh-agent bash -c 'ssh-add /home/aaron/.ssh/github; git pull origin master --force --recurse-submodules'
+hexo clean
 hexo generate
