e4f689ef337e1780c89fd3677de766043f78d465
Aaron Griffin <aig787@gmail.com>
2015-01-24 00:50:27 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+ssh-agent sh -c 'ssh-add /home/aaron/.ssh/github_id_rsa; git pull origin master'
 hexo clean
 hexo generate
 mkdir public/.well-known
-mv ~/keybase.txt public/.well-known/keybase.txt
+cp ~/keybase.txt public/.well-known/keybase.txt
