c8d4da0f18522c2f13c020b178eca51cba539cb6
Aaron Griffin <aig787@gmail.com>
2016-07-14 01:25:44 -0400

    Updated hexo version

--- README.md
+++
@@ -1,6 +0,0 @@
-aarongriffin.ninja
-=====================
-
-Personal website for posts about security, programming, and about me.
-
-[https://aarongriffin.ninja](https://aarongriffin.ninja)
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+cp -r public/.well-known ~/.well-known
 ssh-agent sh -c 'ssh-add /home/aaron/.ssh/github_id_rsa; git pull origin master'
 hexo clean
 hexo generate
-mkdir public/.well-known
-cp ~/keybase.txt public/.well-known/keybase.txt
+cp ~/.well-known public/.well-known
